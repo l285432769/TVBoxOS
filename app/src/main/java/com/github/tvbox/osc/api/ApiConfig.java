eb3ceef6a9ab7de875fe4e8ba963e18d87e6f366
l285432769 <285432769@qq.com>
2025-05-10 21:07:07 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -172,7 +172,7 @@ public class ApiConfig {
         return configUrl;
     }
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gh-proxy.com/raw.githubusercontent.com/285432769l/TVBox/refs/heads/main/XYQTVBox.json");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gh-proxy.com/raw.githubusercontent.com/l285432769/TVBox/refs/heads/main/XYQTVBox.json");
         //独立加载直播配置
         String liveApiUrl = Hawk.get(HawkConfig.LIVE_API_URL, "");
         String liveApiConfigUrl=configUrl(liveApiUrl);
